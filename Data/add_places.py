f61cdf9d10c3422d7ff52af8e9ae3f313cf71480
Ankush Singh Rawat <140985139+ankush850@users.noreply.github.com>
2025-08-27 17:41:16 +0530

    Update add_places.py

--- Data/add_places.py
+++ Data/add_places.py
@@ -28,3 +28,33 @@ for r in tr:
         'country': country
     }
     df = df.append(row, ignore_index=True)
+
+
+# Adding top 30 airports in india
+page = request.urlopen("https://www.worlddata.info/asia/india/airports.php")
+soup = bs(page, features="html.parser")
+
+tr = soup.body.find_all('table')[0].find_all('tr')
+
+for r in tr[1:]:
+    d = r.find_all('td')
+    airport = d[1].text.strip()
+    code = d[0].text.strip().upper()
+    city = d[2].text.strip()
+    country = 'India'
+    
+    row = {
+        'city': city,
+        'airport': airport,
+        'code': code,
+        'country': country
+    }
+    if len(df[df['code'] == code]):
+        continue
+    else:
+        df = df.append(row, ignore_index=True)
+
+print("Saving data in the file airports.csv")
+df.to_csv("airports.csv", index=False)
+
+print("DONE")
